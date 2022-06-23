a2a3847e6ec25625857d59f1ce7620c2747d3985
ok1341 <ole2809@gmail.com>
2022-06-23 12:19:20 +0200

    filed added

---
+++ model_test.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle 
+import random
+from urllib.request import urlopen
+import requests
+from io import BytesIO
+import pandas as pd
+import cloudpickle as cp
+import streamlit as st
+
+
+#load data
+
+features = pickle.load(open('./images1.pkl', 'rb'))
+
+words_to_index = 'wget -O https://github.com/ok1341/Image-Captioning-files/blob/eee94e6288157c94532f50208ca89af5be6cf31c/words.pkl'
+index_to_words = 'wget -O https://github.com/ok1341/Image-Captioning-files/blob/eee94e6288157c94532f50208ca89af5be6cf31c/words1.pkl'
+
+model = 'wget -O https://github.com/ok1341/Image-Captioning-files/blob/eee94e6288157c94532f50208ca89af5be6cf31c/model_14.h5'
+
+
+#features = pickle.load(requests.get("https://drive.google.com/file/d/1F8FGdOdCOYiNRcFxZdhKzaXcJSFUOaW1/view?usp=sharing", stream=True))
+#words_to_index = cp.load(urlopen("https://github.com/ok1341/Image-Captioning-files/blob/eee94e6288157c94532f50208ca89af5be6cf31c/words.pkl"))
+
+st.cache
+images = "wget -O https://github.com/ok1341/Image-Captioning-files/blob/eee94e6288157c94532f50208ca89af5be6cf31c/Images/"
+
+max_length = 33
+
+
+#generate captions
+def image_captioning(picture):
+    in_text = 'startseq'
+    for i in range(max_length):
+        sequence = [words_to_index[w] for w in in_text.split() if w in words_to_index]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        yhat = model.predict([picture,sequence], verbose=0)
+        yhat = np.argmax(yhat)
+        word = index_to_words[yhat]
+        in_text += ' ' + word
+        if word == 'endseq':
+            break
+    final = in_text.split()
+    final = final[1:-1]
+    final = ' '.join(final)
+    return final
+
+def org_caption(id):
+    with open("captions.txt", 'r') as f:
+        caps = [line.rstrip() for line in f]
+        original = caps[id]
+    return original
+
+
+#show picture an caption
+def show():
+    z = random.randint(0,20)
+    pic = list(features.keys())[z]
+    image = features[pic].reshape((1,2048))
+    x = plt.imread(images + pic)
+    st.image(x)
+    st.write("Generated caption:", image_captioning(image))
+    st.write("Original caption:", org_caption(z + 1))
+    
+if st.button("Generate caption"):
+    show()
+ 
