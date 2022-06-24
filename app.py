2287b115e505ecf3affeae9aa426c85d30581875
Moritz Eckardt <83656997+moritzeckardt@users.noreply.github.com>
2022-06-24 14:52:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,10 +20,11 @@ st.write("# ML4B - Image Caption Generator")
 # Project explanation
 st.header("Short project explanation")
 st.write("""Hello, we are Jonas, Moritz and Ole. Together we have set ourselves the goal of building, explaining and 
-presenting an image caption generator using Deep Learning and its Neural Networks. We use these subtype of machine 
-learning, as it is the closest to the way humans analyze images. Based on this, we want to compare our own generated 
-caption with the actual caption of the image. The tip of the iceberg would be if we manage to algorithmically 
-evaluate the appropriateness of our caption.""")
+presenting an image caption generator using Deep Learning and its Neural Networks. We use this subtype of machine 
+learning as it is the closest to the way humans analyze images. The user has the option to upload any image to which
+our model will generate a caption as accurate as possible. After that, the user can vote whether the original or 
+generated caption better reflects the image content. All results are displayed by means of a chart for further 
+evaluation.""")
 st.write("")
 
 # Data preparation
