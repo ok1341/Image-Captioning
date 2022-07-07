40174c1e2e1c3eccecebafd3ec1e8102553f0293
Moritz Eckardt <83656997+moritzeckardt@users.noreply.github.com>
2022-07-07 22:15:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,40 +29,6 @@ of the two captions they think better represents the image content. The results 
 and further analysis.""")
 st.write("")
 
-# Data preparation
-st.header("Our data preparation process")
-
-with st.expander("1. Data Understanding"):
-    st.write("""For our project we are using the LAION data set, which is available to us as a CSV file. It is currently 
-    the largest freely accessible image-text dataset in the world (240TB). The CSV file contains various attributes, 
-    for example URL, TEXT, NSFW or Similarity. The URL can be used to load numerous images and display them. 
-    The data set sets the ground for our goal of generating image captions. Of course it is too large in its actual 
-    form, which is why we decided to use a smaller sample. We agreed on a sample size of about 1,000 - 5,000 examples. 
-    At the moment, we have focused on the following 3 image themes when training the model: Cars, Chairs and Couches. 
-    One of the problems with our project could be the comparison of the captions, because some of the captions are very 
-    specific and do not always reflect the actual content of the images.""")
-
-with st.expander("2. Source Selection"):
-    st.write("""As already explained in the previous step, we make use of the LAION data set. The total data for 
-    training, validating and testing will initially be 1,000 - 5,000 images to not make the process too time-consuming.
-    We are not concerned about missing data sources with this data set.""")
-
-with st.expander("3. Data Cleaning"):
-    st.write("""Corrupted data cannot be found in our data set. In rare cases, an image cannot be be loaded. However, 
-    in our opinion there is nothing to be done about this problem. We would like to clean our images for english 
-    captions, NSFW, a maximum caption length as well as appropriate keywords.""")
-
-with st.expander("4. Feature Engineering"):
-    st.write("""In terms of feature engineering, we might use the typical techniques for images:
-     Resizing, cropping, clipping, blur, etc. We keep our options open in this area and analyze what could be 
-     useful.""")
-
-with st.expander("5. Data Splitting"):
-    st.write("""For the time being, we would like to start with a total of about 5,000 images. These will be divided 
-    into a Train Set (70-80%), Validation Set (10-15%) and Test Set (10-15%). The process of dividing will be 
-    random.""")
-st.write("")
-
 # Show picture and generate caption
 def gen_caption(picture):
     st.image(picture)
